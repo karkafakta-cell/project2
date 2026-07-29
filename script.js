cf8d22aad04d2987970db970848f9ea902168023
hazfifakta-cell <karkafakta@gmail.com>
2026-07-29 15:22:18 +0800

    pindah tata letak

--- script.js
+++ script.js
@@ -406,15 +406,10 @@ async function kirimDataKeSupabase() {
     // === KODE BARU: AMBIL DATA DISCORD DARI MEMORI SEMENTARA BROWSER ===
     const dataDiscordSementara = JSON.parse(sessionStorage.getItem("discord_user"));
     
-    if (dataDiscordSementara && dataDiscordSementara.avatar) {
-    fotoProfilFinal = `https://discordapp.com/avatars/${dataDiscordSementara.id}/${dataDiscordSementara.avatar}.png`;
-    }
-
-
     // === KODE BARU: BUAT ALAMAT FOTO PROFIL DISCORD YANG BENAR ===
     let fotoProfilFinal = avatar; // Default menggunakan input manual jika ada
     if (dataDiscordSementara && dataDiscordSementara.avatar) {
-        fotoProfilFinal = `https://discordapp.com{dataDiscordSementara.id}/${dataDiscordSementara.avatar}.png`;
+    fotoProfilFinal = `https://discordapp.com/avatars/${dataDiscordSementara.id}/${dataDiscordSementara.avatar}.png`;
     }
 
     // === KODE BARU: RAKIT OBJEK DATA GABUNGAN FORM + DISCORD ===
